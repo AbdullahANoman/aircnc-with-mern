249fec649e7954fcee256057b3cb079bafe555d5
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-05-31 09:49:10 +0600

    added categories to implement

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Container from "../Shared/Container/Container";
+
+const Categories = () => {
+  return (
+    <Container>
+      <div>
+        <p>This is category</p>
+      </div>
+    </Container>
+  );
+};
+
+export default Categories;
