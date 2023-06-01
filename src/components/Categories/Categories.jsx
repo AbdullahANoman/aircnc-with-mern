ec777ab95cf9514d913e2bdfe6c799565b45baee
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-06-01 08:54:11 +0600

    add category data and row

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,12 +1,16 @@
 
 import React from "react";
 import Container from "../Shared/Container/Container";
+import { categories } from "./categoriesData";
+import CategoryBox from "./CategoryBox";
 
 const Categories = () => {
   return (
     <Container>
-      <div>
-        <p>This is category</p>
+      <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+        {
+          categories.map((item,index)=> <CategoryBox item={item} key={index}></CategoryBox>)
+        }
       </div>
     </Container>
   );
